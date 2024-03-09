c17455c0cea36f9d8412da75f9b10ebc5e5522b5
Sf-Shohag71 <shakhfarid71@gmail.com>
2024-03-09 13:12:40 +0600

    Implemented hero section

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+export default function Button({label, iconURL}) {
+  return (
+    <button className="flex justify-center items-center px-7 py-4 rounded-full bg-coral-red gap-2 text-lg text-white leading-none border border-coral-red font-montserrat">
+        {label}
+        <img src={iconURL} alt="arrow right" className="ml-2 rounded-full h-5 w-5" />
+    </button>
+  )
+}
