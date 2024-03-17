f779bd9d11f915eb07b6484e51a31542b27f5105
Sf-Shohag71 <shakhfarid71@gmail.com>
2024-03-17 12:33:09 +0600

    Ïmplemented responsive navbar for mobile device

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,41 +1,76 @@
-import { hamburger } from "../assets/icons";
+import { useState } from "react";
+import { cross, hamburger } from "../assets/icons";
 import { siteLogo } from "../assets/images";
 
 import { navLinks } from "../constants";
 
 const Nav = () => {
+  const [toggleNav, setToggleNav] = useState(false);
+  const handleNavbar = () => {
+    setToggleNav(!toggleNav);
+  };
   return (
-    <header className='padding-x py-8 absolute z-10 w-full'>
-      <nav className='flex justify-between items-center max-container'>
-        <a href='/'>
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
           <img
             src={siteLogo}
-            alt='logo'
+            alt="logo"
             width={120}
             height={60}
-            className='m-0 w-[120px] h-[60px]'
+            className="m-0 w-[120px] h-[60px]"
           />
         </a>
-        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
           {navLinks.map((item) => (
             <li key={item.label}>
               <a
                 href={item.href}
-                className='font-montserrat leading-normal text-lg text-slate-gray'
+                className="font-montserrat leading-normal text-lg text-slate-gray"
               >
                 {item.label}
               </a>
             </li>
           ))}
         </ul>
-        <div className='flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24'>
-          <a href='/'>Sign in</a>
+        <div className="flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24">
+          <a href="/">Sign in</a>
           <span>/</span>
-          <a href='/'>Explore now</a>
+          <a href="/">Explore now</a>
         </div>
-        <div className='hidden max-lg:block'>
-          <img src={hamburger} alt='hamburger icon' width={25} height={25} />
+        {/* Navbar for mobile device */}
+        <div className="block md:hidden">
+          <button onClick={handleNavbar}>
+            {toggleNav ? (
+              <img src={cross} alt="menu closing icon" width={25} height={25} />
+            ) : (
+              <img
+                src={hamburger}
+                alt="hamburger icon"
+                width={25}
+                height={25}
+              />
+            )}
+          </button>
         </div>
+        <ul
+          className={
+            toggleNav
+              ? "fixed md:hidden left-0 top-0 pt-10 pl-6 w-[60%] h-full border-r border-r-gray-900 bg-black ease-in-out duration-500"
+              : "ease-in-out w-[60%] duration-500 fixed top-0 pt-10 pl-6 bottom-0 left-[-100%]"
+          }
+        >
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-gray-50 hover:text-white-400"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
       </nav>
     </header>
   );
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -24,7 +24,7 @@ const hero = () => {
             The New Arrival
           </span>
           <br />
-          <span className="text-coral-red inline-block mt-3">SF</span> Shoes
+          <span className="text-coral-red inline-block mt-3">Shoes</span> Collections
         </h1>
         <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
           Explore the latest SF shoe arrivals, experience exceptional comfort,
